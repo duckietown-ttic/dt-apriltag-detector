f01a20f2eef30a0f242633a73540938e5e0f884d
Andrea F. Daniele <afdaniele@ttic.edu>
2020-09-09 16:59:04 -0500

    going raycasting, first working prototype w/ multiprocessing

--- packages/simulator/include/apriltag_simulator/Camera.py
+++ packages/simulator/include/apriltag_simulator/Camera.py
@@ -1,12 +1,18 @@
+import math
+from itertools import product
+from multiprocessing.pool import Pool
+from threading import Thread
+
 import numpy as np
 
 from skimage.morphology import flood_fill
 
-from apriltag_simulator.lenses import CameraLens
 from apriltag_simulator.utils import ProgressBar
 from apriltag_simulator.exceptions import ObjectOutOfXBounds, ObjectOutOfYBounds
+from apriltag_simulator.utils import transformations
 
 INF = 9999999999
+np.set_printoptions(suppress=True)
 
 
 class Camera(object):
@@ -29,7 +35,7 @@ class Camera(object):
             [0,         self._fy,    self._cy],
             [0,                0,           1]
         ])
-        self._IO = np.array([
+        self._Rt = np.array([
             [1,    0,    0,    0],
             [0,    1,    0,    0],
             [0,    0,    1,    0]
@@ -37,8 +43,42 @@ class Camera(object):
         self._width = width
         self._height = height
         self._lens = None
+        self._CRt = np.matmul(self._C, self._Rt)
+
+    @property
+    def cx(self):
+        return self._cx
+
+    @property
+    def cy(self):
+        return self._cy
+
+    @property
+    def fx(self):
+        return self._fx
+
+    @property
+    def fy(self):
+        return self._fy
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def height(self):
+        return self._height
+
+    @property
+    def C(self):
+        return self._C
+
+    @property
+    def lens(self):
+        return self._lens
 
     def attach_lens(self, lens):
+        from apriltag_simulator.lenses import CameraLens
         if not isinstance(lens, CameraLens):
             raise ValueError(
                 'Object `lens` must be of type CameraLens, got %s instead' % str(type(lens)))
@@ -47,135 +87,200 @@ class Camera(object):
     def detach_lens(self):
         self._lens = None
 
-    def capture(self, point3, no_lens=False):
-        if isinstance(point3, np.ndarray):
-            point3 = point3.tolist()
-        point3 = np.array(point3 + [1])
-        # apply lens distortion
-        if self._lens is not None and not no_lens:
-            if point3[2] == 0:
-                return None, None
-            xn = point3[0:2] / point3[2]
-            dpoint2 = self._lens.transform(xn)
-            point2 = np.matmul(self._C, dpoint2.tolist() + [1])
-            point2 = point2[0:2]
-        else:
-            point2 = np.matmul(np.matmul(self._C, self._IO), point3)
-            if point2[2] == 0:
-                return None, None
-            point2 = point2[0:2] / point2[2]
+    # def capture(self, point3, no_lens=False):
+    #     if isinstance(point3, np.ndarray):
+    #         point3 = point3.tolist()
+    #     point3 = np.array(point3 + [1])
+    #
+    #
+    #     # apply lens distortion
+    #     if self._lens is not None and not no_lens:
+    #         if point3[2] == 0:
+    #             return None, None
+    #         xn = point3[0:2] / point3[2]
+    #         # print(f'{np.round(point3, 4)} -N-> {np.round(xn, 4)}')
+    #         dpoint2 = self._lens.transform(xn)
+    #         # print(f'{np.round(xn, 4)} -D-> {np.round(dpoint2, 4)}')
+    #         point2 = np.matmul(self._C, dpoint2.tolist() + [1])
+    #         # print(f'{np.round(dpoint2, 4)} -P-> {np.round(point2, 4)}')
+    #         point2 = point2[0:2]
+    #     else:
+    #         point2 = np.matmul(self._CRt, point3)
+    #         if point2[2] == 0:
+    #             return None, None
+    #         point2 = point2[0:2] / point2[2]
+    #     return point2
+    #
+    #
+    #
+    #
+    #
+    #     # point2 = np.matmul(self._CRt, point3)
+    #     # if point2[2] == 0:
+    #     #     return None, None
+    #     # point2 = point2[0:2] / point2[2]
+    #     #
+    #     # point2 = np.array([(point2[0] - self._cx) / self._fx, (point2[1] - self._cy) / self._fy])
+    #     #
+    #     # # apply lens distortion
+    #     # dpoint2 = self._lens.transform(point2)
+    #     #
+    #     # dpoint2 = [dpoint2[0] * self._fx + self._cx, dpoint2[1] * self._fy + self._cy]
+    #     #
+    #     # # print(f'{point2} -D-> {dpoint2}')
+    #     #
+    #     # point2 = dpoint2
+    #
+    #
+    #     # ---
+    #     return point2
+    #
+    # def render(self, scene, bgcolor=None, scream=False, progress=False, refine=True, return_stats=False):
+    #     if bgcolor is None:
+    #         bgcolor = 0
+    #
+    #     img = np.full((self._width, self._height, 3), -1)
+    #     stats = {
+    #         'min_distortion_px': INF,
+    #         'max_distortion_px': -INF,
+    #         'horizontal_fov_deg': 0,
+    #         'vertical_fov_deg': 0,
+    #         'diagonal_fov_deg': 0,
+    #         'rectified_uv_domain': {
+    #             'u': [INF, -INF],
+    #             'v': [INF, -INF]
+    #         }
+    #     }
+    #
+    #     for obj in scene:
+    #         pbar = ProgressBar(header='Object: %s' % obj.name)
+    #         num_points = obj.num_points()
+    #         # iterate over the object points
+    #         i = 0
+    #         for point3, color3 in obj.points():
+    #             u, v = self.capture(point3)
+    #
+    #             # print(f'[{np.round(point3, 3)}] -> [{u}, {v}]')
+    #
+    #             if u is None:
+    #                 continue
+    #             u, v = np.array([u, v]).round().astype(np.int32).tolist()
+    #
+    #             if progress:
+    #                 pbar.update(100 * i / num_points)
+    #             if 0 <= u < self._width and 0 <= v < self._height:
+    #                 img[u, v] = color3
+    #
+    #                 if return_stats:
+    #                     # compute minimum/maximum distortion
+    #                     u1, v1 = self.capture(point3, no_lens=True)
+    #                     if u1 is None:
+    #                         continue
+    #                     u1, v1 = np.array([u1, v1]).round().astype(np.int32).tolist()
+    #
+    #                     distortion = np.linalg.norm([u - u1, v - v1])
+    #                     stats['min_distortion_px'] = min(stats['min_distortion_px'], distortion)
+    #                     stats['max_distortion_px'] = max(stats['max_distortion_px'], distortion)
+    #                     # compute effective FoV
+    #                     stats['horizontal_fov_deg'] = max(
+    #                         stats['horizontal_fov_deg'],
+    #                         2 * np.rad2deg(np.arctan(point3[0] / point3[2]))
+    #                     )
+    #                     stats['vertical_fov_deg'] = max(
+    #                         stats['vertical_fov_deg'],
+    #                         2 * np.rad2deg(np.arctan(point3[1] / point3[2]))
+    #                     )
+    #                     stats['diagonal_fov_deg'] = np.linalg.norm(
+    #                         [stats['horizontal_fov_deg'], stats['vertical_fov_deg']])
+    #                     # compute domains for u, v in rectified image
+    #                     stats['rectified_uv_domain']['u'][0] = min(
+    #                         stats['rectified_uv_domain']['u'][0], u1)
+    #                     stats['rectified_uv_domain']['u'][1] = max(
+    #                         stats['rectified_uv_domain']['u'][1], u1)
+    #                     stats['rectified_uv_domain']['v'][0] = min(
+    #                         stats['rectified_uv_domain']['v'][0], v1)
+    #                     stats['rectified_uv_domain']['v'][1] = max(
+    #                         stats['rectified_uv_domain']['v'][1], v1)
+    #             else:
+    #                 if scream and (u < 0 or u >= self._width) and np.sum(color3) == 0:
+    #                     if progress:
+    #                         pbar.done()
+    #                     raise ObjectOutOfXBounds()
+    #                 if scream and (v < 0 or v >= self._height) and np.sum(color3) == 0:
+    #                     if progress:
+    #                         pbar.done()
+    #                     raise ObjectOutOfYBounds()
+    #
+    #             i += 1
+    #
+    #         if progress:
+    #             pbar.done()
+    #
+    #     if refine:
+    #         # fill background pixels
+    #         corners = [(0, 0), (self._width-1, 0), (self._width-1, self._height-1), (0, self._height-1)]
+    #         selem = [[0, 1, 0], [1, 0, 1], [0, 1, 0]]
+    #         for u, v in corners:
+    #             if img[u, v, 0] == -1:
+    #                 img[:, :, 0] = flood_fill(img[:, :, 0], (u, v), bgcolor, selem=selem)
+    #                 img[:, :, 1] = flood_fill(img[:, :, 1], (u, v), bgcolor, selem=selem)
+    #                 img[:, :, 2] = flood_fill(img[:, :, 2], (u, v), bgcolor, selem=selem)
+    #
+    #         # find all -1 pixels
+    #         neigh_mask = [
+    #             [-1, -1], [0, -1], [1, -1],
+    #             [-1,  0],          [1,  0],
+    #             [-1,  1], [0,  1], [1,  1]
+    #         ]
+    #         empty_pixels = np.argwhere(img == -1)[:, 0:2]
+    #         indices = np.unique(empty_pixels, axis=0) if len(empty_pixels) else []
+    #
+    #         # fill -1 pixels with the most common color among the neighboring ones
+    #         for pixel in indices:
+    #             neighs = np.add(neigh_mask, pixel)
+    #             neighs = [n for n in neighs if 0 <= n[0] < self._width and 0 <= n[1] < self._height]
+    #             colors = img[tuple(np.array(neighs).T)]
+    #             colors, count = np.unique(colors, axis=0, return_counts=True)
+    #             color = colors[np.argmax(count)]
+    #             img[pixel[0], pixel[1]] = color
+    #     # ---
+    #     img = img.astype(np.uint8)
+    #     if return_stats:
+    #         return img, stats
+    #     return img
+
+    def render(self, scene, bgcolor=None, num_threads=4, return_stats=False, progress=False):
+        img = np.full((self._width, self._height, 3), bgcolor)
+        # split rendering job to num_threads workers
+        pool = Pool(num_threads)
+        args = []
+        for i in range(num_threads):
+            args.append((i, num_threads, self, scene))
+        # spin workers
+        res = pool.starmap(ray_casting_task, args)
+        # combine results
+        for ma in res:
+            img = np.ma.where(ma == -1, img, ma)
         # ---
-        return point2
-
-    def render(self, scene, bgcolor=None, scream=False, progress=False, refine=True, return_stats=False):
-        if bgcolor is None:
-            bgcolor = 0
-
-        img = np.full((self._width, self._height, 3), -1)
-        stats = {
-            'min_distortion_px': INF,
-            'max_distortion_px': -INF,
-            'horizontal_fov_deg': 0,
-            'vertical_fov_deg': 0,
-            'diagonal_fov_deg': 0,
-            'rectified_uv_domain': {
-                'u': [INF, -INF],
-                'v': [INF, -INF]
-            }
-        }
-
-        for obj in scene:
-            pbar = ProgressBar(header='Object: %s' % obj.name)
-            num_points = obj.num_points()
-            # iterate over the object points
-            i = 0
-            for point3, color3 in obj.points():
-                u, v = self.capture(point3)
-                if u is None:
-                    continue
-                u, v = np.array([u, v]).round().astype(np.int32).tolist()
-
-                if progress:
-                    pbar.update(100 * i / num_points)
-                if 0 <= u < self._width and 0 <= v < self._height:
-                    img[u, v] = color3
-
-                    if return_stats:
-                        # compute minimum/maximum distortion
-                        u1, v1 = self.capture(point3, no_lens=True)
-                        if u1 is None:
-                            continue
-                        u1, v1 = np.array([u1, v1]).round().astype(np.int32).tolist()
-
-                        distortion = np.linalg.norm([u - u1, v - v1])
-                        stats['min_distortion_px'] = min(stats['min_distortion_px'], distortion)
-                        stats['max_distortion_px'] = max(stats['max_distortion_px'], distortion)
-                        # compute effective FoV
-                        stats['horizontal_fov_deg'] = max(
-                            stats['horizontal_fov_deg'],
-                            2 * np.rad2deg(np.arctan(point3[0] / point3[2]))
-                        )
-                        stats['vertical_fov_deg'] = max(
-                            stats['vertical_fov_deg'],
-                            2 * np.rad2deg(np.arctan(point3[1] / point3[2]))
-                        )
-                        stats['diagonal_fov_deg'] = np.linalg.norm(
-                            [stats['horizontal_fov_deg'], stats['vertical_fov_deg']])
-                        # compute domains for u, v in rectified image
-                        stats['rectified_uv_domain']['u'][0] = min(
-                            stats['rectified_uv_domain']['u'][0], u1)
-                        stats['rectified_uv_domain']['u'][1] = max(
-                            stats['rectified_uv_domain']['u'][1], u1)
-                        stats['rectified_uv_domain']['v'][0] = min(
-                            stats['rectified_uv_domain']['v'][0], v1)
-                        stats['rectified_uv_domain']['v'][1] = max(
-                            stats['rectified_uv_domain']['v'][1], v1)
-                else:
-                    if scream and (u < 0 or u >= self._width) and np.sum(color3) == 0:
-                        if progress:
-                            pbar.done()
-                        raise ObjectOutOfXBounds()
-                    if scream and (v < 0 or v >= self._height) and np.sum(color3) == 0:
-                        if progress:
-                            pbar.done()
-                        raise ObjectOutOfYBounds()
-
-                i += 1
-
-            if progress:
-                pbar.done()
-
-        if refine:
-            # fill background pixels
-            corners = [(0, 0), (self._width-1, 0), (self._width-1, self._height-1), (0, self._height-1)]
-            selem = [[0, 1, 0], [1, 0, 1], [0, 1, 0]]
-            for u, v in corners:
-                if img[u, v, 0] == -1:
-                    img[:, :, 0] = flood_fill(img[:, :, 0], (u, v), bgcolor, selem=selem)
-                    img[:, :, 1] = flood_fill(img[:, :, 1], (u, v), bgcolor, selem=selem)
-                    img[:, :, 2] = flood_fill(img[:, :, 2], (u, v), bgcolor, selem=selem)
-
-            # find all -1 pixels
-            neigh_mask = [
-                [-1, -1], [0, -1], [1, -1],
-                [-1,  0],          [1,  0],
-                [-1,  1], [0,  1], [1,  1]
-            ]
-            empty_pixels = np.argwhere(img == -1)[:, 0:2]
-            indices = np.unique(empty_pixels, axis=0) if len(empty_pixels) else []
-
-            # fill -1 pixels with the most common color among the neighboring ones
-            for pixel in indices:
-                neighs = np.add(neigh_mask, pixel)
-                neighs = [n for n in neighs if 0 <= n[0] < self._width and 0 <= n[1] < self._height]
-                colors = img[tuple(np.array(neighs).T)]
-                colors, count = np.unique(colors, axis=0, return_counts=True)
-                color = colors[np.argmax(count)]
-                img[pixel[0], pixel[1]] = color
-        # ---
-        img = img.astype(np.uint8)
-        if return_stats:
-            return img, stats
         return img
 
 
+def ray_casting_task(cur, tot, camera, scene):
+    split = 1 / tot
+    vs = range(int(cur * split * camera.height), int((cur + 1) * split * camera.height), 1)
+    img = np.full((camera.width, camera.height, 3), -1)
+    invM = transformations.inverse_matrix(camera.C)
+    # shoot rays
+    for _u in range(camera.width):
+        for _v in vs:
+            u, v = camera.lens.to_pinhole_pixel(_u, _v)
+            ray = np.matmul(invM, [u, v, 1])
+            z, c = None, None
+            for obj in scene:
+                inters_w, color = obj.intersect(ray)
+                if inters_w and (z is None or inters_w[2] < z):
+                    z, c = inters_w[2], color
+            if c is not None:
+                img[_u, _v] = c
+    # ---
+    return img
--- packages/simulator/include/apriltag_simulator/lenses/FishEyeLens.py
+++ packages/simulator/include/apriltag_simulator/lenses/FishEyeLens.py
@@ -1,4 +1,10 @@
+import math
+import time
+from itertools import product, chain
+import matplotlib.pyplot as plt
+
 from .CameraLens import CameraLens
+from apriltag_simulator.Camera import Camera
 
 import numpy as np
 
@@ -6,11 +12,11 @@ import numpy as np
 class FishEyeLens(CameraLens):
 
     @staticmethod
-    def from_camera_info(name, camera_info):
+    def from_camera_info(name, camera, camera_info):
         K1, K2, P1, P2, K3 = camera_info['distortion_coefficients']['data']
-        return FishEyeLens(name, K1, K2, K3, P1, P2)
+        return FishEyeLens(name, camera, K1, K2, K3, P1, P2)
 
-    def __init__(self, name, k1=0, k2=0, k3=0, p1=0, p2=0):
+    def __init__(self, name, camera, k1=0, k2=0, k3=0, p1=0, p2=0):
         CameraLens.__init__(self, name)
         self._name = name
         self._k1 = k1
@@ -18,8 +24,15 @@ class FishEyeLens(CameraLens):
         self._k3 = k3
         self._p1 = p1
         self._p2 = p2
+        self._camera = camera
+        self._map = self._create_map()
+
+    def to_pinhole_pixel(self, u, v):
+        return self._map[u, v]
 
     def transform(self, point2):
+        if not isinstance(point2, np.ndarray):
+            point2 = np.array(point2)
         x, y = point2.tolist()
         r = np.linalg.norm(point2)
         radial = (1 + self._k1 * (r ** 2) + self._k2 * (r ** 4) + self._k3 * (r ** 6)) * point2
@@ -28,3 +41,155 @@ class FishEyeLens(CameraLens):
             2 * self._p2 * x * y + self._p1 * ((r ** 2) + 2 * (y ** 2))
         ])
         return np.add(radial, tangential)
+
+    def _distort(self, u, v):
+        i = (u - self._camera.cx) / self._camera.fx
+        j = (v - self._camera.cy) / self._camera.fy
+        i, j = self.transform([i, j])
+        _u = int(math.floor(i * self._camera.fx + self._camera.cx))
+        _v = int(math.floor(j * self._camera.fy + self._camera.cy))
+        if 0 <= _u < self._camera.width and 0 <= _v < self._camera.height:
+            return _u, _v
+        return None, None
+
+    def _create_map(self):
+        lens = np.full((self._camera.width, self._camera.height, 2), -1)
+        lensc = np.full((self._camera.width, self._camera.height, 2), -1)
+
+
+        # img = np.full((self._camera.width, self._camera.height, 3), 255)
+        # plot = plt.imshow(img.transpose((1, 0, 2)))
+        # plt.show(block=False)
+
+
+        # find inner and outer contours
+        i = 2
+        stop = False
+        ratio = self._camera.height / self._camera.width
+        innerW, innerH, outerW, outerH = None, None, None, None
+        minX, maxX, minY, maxY = None, None, None, None
+
+        while not stop:
+            w, h = i, int(math.floor(i * ratio))
+            # compute corners
+            w2 = int(math.floor(w * 0.5))
+            h2 = int(math.floor(h * 0.5))
+
+            frame = chain(
+                # top line
+                product(range(-w2, w2, 1), [-h2]),
+                # left line
+                product([-w2], range(-h2 + 1, h2 - 1, 1)),
+                # right line
+                product([w2], range(-h2 + 1, h2 - 1, 1)),
+                # bottom line
+                product(range(-w2, w2, 1), [h2])
+            )
+
+            img = np.full((self._camera.width, self._camera.height, 3), 255)
+
+            hits = 0
+            frame_size = 0
+            for p in frame:
+                frame_size += 1
+                px = np.add(p, [self._camera.width * 0.5, self._camera.height * 0.5]).astype(int)
+                u, v = self._distort(*px)
+                if u is None or v is None:
+                    continue
+                if not (0 <= u < self._camera.width and 0 <= v < self._camera.height):
+                    continue
+                # ---
+                hits += 1
+
+                img[u, v, :] = 0
+                if 0 <= px[0] < self._camera.width and 0 <= px[1] < self._camera.height:
+                    img[px[0], px[1]] = [255, 0, 0]
+
+                # store min and max for X and Y
+                minX = px[0] if minX is None else min(minX, px[0])
+                maxX = px[0] if maxX is None else max(maxX, px[0])
+                minY = px[1] if minY is None else min(minY, px[1])
+                maxY = px[1] if maxY is None else max(maxY, px[1])
+            # ---
+            if hits > 0:
+                # outer frame candidate
+                outerW = maxX - minX
+                outerH = maxY - minY
+            if hits == frame_size:
+                # inner frame candidate
+                innerW = maxX - minX
+                innerH = maxY - minY
+            if hits == 0:
+                # outer frame reached
+                stop = True
+
+            # plot.set_data(img)
+            # plt.pause(0.0001)
+
+            i += 8
+
+        print([outerW, outerH])
+        print([innerW, innerH])
+
+        width, height = outerW, outerH
+
+        plt.close()
+
+
+        # create new temporary camera
+        # # TODO: it might be that we have to leave these unscaled
+        # nfx = width * (self._camera.fx / self._camera.width)
+        # nfy = height * (self._camera.fy / self._camera.height)
+
+        ncx = width * (self._camera.cx / self._camera.width)
+        ncy = height * (self._camera.cy / self._camera.height)
+
+        # print(f'{self._camera.cx}  ->  {ncx}')
+        # print(f'{self._camera.cy}  ->  {ncy}')
+
+        # ncamera = Camera('lens_camera', nfx, nfy, ncx, ncy, width, height)
+        # ncamera = Camera('lens_camera', self._camera.fx, self._camera.fy, ncx, ncy, width, height)
+
+
+        for u in range(width):
+            for v in range(height):
+                i = (u - ncx) / self._camera.fx
+                j = (v - ncy) / self._camera.fy
+                i, j = self.transform([i, j])
+
+
+                _u = int(math.floor(i * self._camera.fx + self._camera.cx))
+                _v = int(math.floor(j * self._camera.fy + self._camera.cy))
+
+                # _u, _v = camera.lens._distort(u, v)
+
+                u1 = u - ncx + self._camera.cx
+                v1 = v - ncy + self._camera.cy
+
+                if _u is not None and _v is not None:
+                    if 0 <= _u < self._camera.width and 0 <= _v < self._camera.height:
+                        lens[_u, _v] = [u1, v1]
+
+        img = np.full((self._camera.width, self._camera.height, 3), 0)
+        img[:, :, 0:2] = 255 * (lens != -1)
+        plt.imshow(img.transpose((1, 0, 2)))
+        plt.show()
+
+        mask_size = 2
+        mask_range = list(range(-mask_size, mask_size + 1, 1))
+        neigh_mask = list(product(mask_range, mask_range))
+
+        empty_pixels = np.argwhere(lens == -1)[:, 0:2]
+        indices = np.unique(empty_pixels, axis=0) if len(empty_pixels) else []
+        for px in indices:
+            neighs = np.add(px, neigh_mask)
+            neighs = [
+                lens[n[0], n[1]] for n in neighs
+                if 0 <= n[0] < self._camera.width and 0 <= n[1] < self._camera.height
+                   and lens[n[0], n[1], 0] != -1
+            ]
+            assert len(neighs) > 0
+            lensc[px[0], px[1]] = np.floor(np.mean(neighs, axis=0)).astype(int)
+
+        # lens = np.ma.where(lens == -1, lensc, lens)
+        return lens.astype(int)
